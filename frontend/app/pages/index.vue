346ea94433c8944cfbd873f841932f1e89d02a94
Daouda <daoudasidibe224@gmail.com>
2025-07-10 20:54:56 +0200

    correction du layout

---
+++ frontend/app/pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="flex h-screen w-full items-center justify-center bg-gray-50">
+    <div class="flex flex-col items-center space-y-4">
+      <div
+        class="h-12 w-12 animate-spin rounded-full border-4 border-solid border-blue-500 border-t-transparent"
+      ></div>
+      <p class="text-lg text-gray-600">Chargement...</p>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+definePageMeta({
+  layout: "empty",
+});
+</script>
